69ede4c3730abc72ab7a885e6bdf3ae30ffe1034
techguyash <techguyash@gmail.com>
2023-11-17 22:09:04 +0530

    azure-changes

--- src/components/axios.js
+++ src/components/axios.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL: "http://localhost:8080/keep",
+  // baseURL: "https://keepclone-keep.azuremicroservices.io/keep",
 });
 
 export default axiosInstance;
